4e806f78ff5529dcd6b5121a4a71ea6ad232f87b
Vladislav Irkin <vladislavirkin@outlook.com>
2022-12-12 23:27:45 +0300

    update

--- src/CodeforcesProblems.Sandbox/Program.cs
+++ src/CodeforcesProblems.Sandbox/Program.cs
@@ -7,69 +7,32 @@ public static class Program
     public static void Main()
     {
         var testCasesCount = ReadIntLine();
-        for (var i = 0; i < testCasesCount[0]; i++)
+        for (var j = 0; j < testCasesCount[0]; j++)
         {
-            var ret = "NO";
-            var numbersCount = ReadIntLine();
             var numbers = ReadIntLine();
-            
-            numbers.Sort();
+            var str = Console.ReadLine();
 
-            var allDivisors = new HashSet<int>();
+            var ret = "NO";
 
-            foreach (var number in numbers)
+            var letterSet = new HashSet<string>();
+
+            for (var i = 0; i < numbers[0] - 1; i++)
             {
-                var divisors = Factorize(number);
-                foreach (var divisor in divisors)
+                var substr = str.Substring(i, 2);
+                if (letterSet.Contains(substr))
                 {
-                    if (allDivisors.Contains(divisor))
-                    {
-                        ret = "YES";
-                        break;
-                    }
-
-                    allDivisors.Add(divisor);
+                    ret = "YES";
+                    break;
                 }
 
-                if (ret == "YES")
-                    break;
+                if (i > 0)
+                    letterSet.Add(str.Substring(i - 1, 2));
             }
             
             Console.WriteLine(ret);
         }
     }
-
-    private static HashSet<int> Factorize(int number)
-    {
-        var ret = new HashSet<int>();
-        
-        if (number % 2 == 0)
-        {
-            ret.Add(2);
-            number /= 2;
-        }
-        
-        for(var i = 3; i * i <= number; i += 2)
-        {
-            while (number % i == 0)
-                number /= i;
-            ret.Add(i);
-        }
-        
-        if (number > 1)
-            ret.Add(number);
-
-        return ret;
-    }
     
-    private static void AddLine(this StringBuilder sb, object s)
-    {
-        sb.Append(s.ToString());
-        sb.Append('\n');
-    }
-    private static void Print(this StringBuilder sb) => Console.Write(sb.ToString());
     private static List<T> ReadLine<T>(Func<string, T> func) => Console.ReadLine()?.Split(' ').Select(func).ToList();
     private static List<int> ReadIntLine() => ReadLine(int.Parse);
-    private static List<long> ReadLongLine() => ReadLine(long.Parse);
-    private static List<ulong> ReadUlongLine() => ReadLine(ulong.Parse);
 }
---
+++ src/Contests/Date_2022-12-12/EducationalContest_139.cs
@@ -0,0 +1,88 @@
+﻿using System.Security.AccessControl;
+using NUnit.Framework;
+
+namespace Contests.Date_2022_12_12;
+
+[TestFixture]
+public class EducationalContest_139
+{
+    [Test]
+    [TestCase(4, "momo", "YES")]
+    [TestCase(8, "labacaba", "YES")]
+    [TestCase(10, "codeforces", "NO")]
+    [TestCase(5, "uohhh", "NO")]
+    [TestCase(16, "isthissuffixtree", "YES")]
+    [TestCase(1, "x", "NO")]
+    public void P01_Notepad(int n, string str, string expected)
+    {
+        var ret = "NO";
+
+        var letterSet = new HashSet<string>();
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            var substr = str.Substring(i, 2);
+            if (letterSet.Contains(substr))
+            {
+                ret = "YES";
+                break;
+            }
+
+            if (i > 0)
+                letterSet.Add(str.Substring(i - 1, 2));
+        }
+
+        Assert.AreEqual(expected, ret, "Calculated incorrectly.");
+    }
+    
+    [Test]
+    [TestCase(100, 19)]
+    [TestCase(9, 9)]
+    [TestCase(42, 13)]
+    [TestCase(13, 10)]
+    [TestCase(111, 19)]
+    [TestCase(5000, 32)]
+    public void P00_Extremely_Round(int number, int expected)
+    {
+        int ret;
+
+        var ordinalCounter = 0; 
+        while (number > 10)
+        {
+            if (number >= 100000)
+            {
+                number /= 100000;
+                ordinalCounter += 5;
+                continue;
+            }
+
+            if (number >= 10000)
+            {
+                number /= 10000;
+                ordinalCounter += 4;
+                continue;
+            }
+            
+            if (number >= 1000)
+            {
+                number /= 1000;
+                ordinalCounter += 3;
+                continue;
+            }
+
+            if (number >= 100)
+            {
+                number /= 100;
+                ordinalCounter += 2;
+                continue;
+            }
+
+            number /= 10;
+            ordinalCounter += 1;
+        }
+
+        ret = number + ordinalCounter * 9;
+
+        Assert.AreEqual(expected, ret, "Calculated incorrectly.");
+    }
+}
